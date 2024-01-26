6925d9f66554020fdf03c9c3ce517e1cb08a6509
Andre Dias <andteixe@student.42porto.com>
2024-01-26 22:03:52 +0000

    add c05 ex06

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,10 +6,36 @@
 /*   By: andteixe <andteixe@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 00:21:24 by andteixe          #+#    #+#             */
-/*   Updated: 2024/01/25 00:21:39 by andteixe         ###   ########.fr       */
+/*   Updated: 2024/01/26 21:33:30 by andteixe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb)
 {
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb <= 3)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
 }
+
+/*
+#include <stdio.h>
+#include <limits.h>
+int	main(void)
+{
+	printf("%d\n", ft_is_prime(INT_MAX));
+	return (0);
+}
+*/

4f25bedf77edc2b088413d7ca070eed08467b22e
whzd <whzdxl@gmail.com>
2024-01-30 01:26:36 +0000

    Add C08 exercises (incomplete)

---
+++ c08/ex00/ft.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andteixe <andteixe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 23:57:08 by andteixe          #+#    #+#             */
+/*   Updated: 2024/01/30 01:02:20 by andteixe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);

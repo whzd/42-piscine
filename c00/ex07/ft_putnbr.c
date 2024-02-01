5d5591ac0d943d1b0a0dc8ce99cb8e931e6ed7aa
whzd <whzdxl@gmail.com>
2024-02-01 02:15:43 +0000

    Update C00 ex07

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,29 +6,48 @@
 /*   By: andteixe <andteixe@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 18:12:58 by andteixe          #+#    #+#             */
-/*   Updated: 2024/01/20 20:13:17 by andteixe         ###   ########.fr       */
+/*   Updated: 2024/02/01 02:15:03 by andteixe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_putnbr(int nbr)
 {
-	long	n;
-
-	n = nbr;
-	if (n < 0)
+	if (nbr == -2147483648)
 	{
-		write(1, "-", 1); 
-		n = -n;
+		ft_putnbr(nbr / 10);
+		ft_putnbr(-(nbr % 10));
 	}
-	if (n >= 10)
-		ft_putnbr(n/10);
-	ft_putnbr(n % 10 + 48);
+	else if (nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+		ft_putnbr(nbr);
+	}
+	else if (nbr / 10 != 0)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + '0');
 }
 
+/*
 int	main(void)
 {
+	ft_putnbr(0);
+	ft_putchar('\n');
 	ft_putnbr(-8);
+	ft_putchar('\n');
 	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	return (0);
 }
+*/
